8a579b480e1b57d53075f590420920ea89b20d7d
jjshen <jjshen@andrew.cmu.edu>
2025-02-25 10:22:57 -0500

    primes

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -47,5 +47,25 @@ export default function QueryProcessor(query: string): string {
     }
   }
 
+  if (query.toLowerCase().includes("which of the following numbers are primes")) {
+    if (numberMatch) {
+      const numbers = numberMatch.map(Number);
+      const isPrime = (num: number) => {
+        if (num < 2) return false;
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+          if (num % i === 0) return false;
+        }
+        return true;
+      };
+
+      const primes = numbers.filter(isPrime);
+      if (primes.length > 0) {
+        return `${primes.join(", ")}`;
+      } else {
+        return "None of the given numbers are prime.";
+      }
+    }
+  }
+
   return "";
 }
